ef9b66f8b42273e2c20fb83273a988efc445d6ac
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    [squash] base: Introduce app lock [1/4]
    base: applock: refactor
    
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    services: applock: allow background authentication
    
    base: merge hidden app feature with applock
    
    services: AppLockConfig: fix a derp in hidePackage()
    
    services: applock: fix data migration and change secure notification parameter name
    
    * also fixed addPackage replacing data without checking if entry already exists
    
    services: AppLockManagerService: use the og calling uid for target intent
    
    * fixes launching app activites not exported
    
    * nift4: avoid changing public api
    
    * neobuddy89: Add privapp permissions for Settings, updated for A14
    
    Change-Id: Ie35c6c7103b8bed644d510d8268d6c99cb71a1a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    services: Fix Applock service initialization
    
    test: boot, app lock works, no ClassNotFound exceptions during bootup
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -126,6 +126,7 @@ import com.android.server.adb.AdbService;
 import com.android.server.alarm.AlarmManagerService;
 import com.android.server.am.ActivityManagerService;
 import com.android.server.ambientcontext.AmbientContextManagerService;
+import com.android.server.app.AppLockManagerService;
 import com.android.server.app.GameManagerService;
 import com.android.server.appbinding.AppBindingService;
 import com.android.server.appfunctions.AppFunctionManagerService;
@@ -2814,6 +2815,10 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(AuthenticationPolicyService.class);
                 t.traceEnd();
             }
+
+            t.traceBegin("AppLockManagerService");
+            mSystemServiceManager.startService(AppLockManagerService.Lifecycle.class);
+            t.traceEnd();
 
             if (!isWatch) {
                 // We don't run this on watches as there are no plans to use the data logged
